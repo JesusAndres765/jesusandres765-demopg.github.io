0486ae8218bb02dd62072424b53fdccba828e75c
JesusAndres765 <jesusaw065@gmail.com>
2026-05-02 14:57:44 -0600

    Primero Commit

---
+++ src/main/java/com/postgres/demopg/controllers/TweetController.java
@@ -0,0 +1,35 @@
+package com.postgres.demopg.controllers;
+import java.util.Optional;
+import jakarta.validation.Valid;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.postgres.demopg.models.Tweet;
+import com.postgres.demopg.repository.TweetRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@CrossOrigin(origins = "*", maxAge =3600)
+@RestController
+@RequestMapping("/api/tweets")
+public class TweetController {
+    @Autowired
+    private TweetRepository tweetRepository;
+
+    @GetMapping("")
+    public Page<Tweet> getTweet(Pageable pageable){
+        return tweetRepository.findAll(pageable);
+    }
+
+    @PostMapping("")
+    public Tweet createTweet(@Valid @RequestBody Tweet tweet){
+        Tweet myTweet = new Tweet(tweet.getTweet());
+        tweetRepository.save(myTweet);
+        return myTweet;
+    }
+}
